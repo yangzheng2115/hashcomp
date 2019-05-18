0494ad48eb0c7ffac6e861e964af1215342937d8
lwhay <lwh@whu.edu.cn>
2019-05-18 16:10:15 +0800

    efficiency boundary.

--- tests/LH_MicroTest.cpp
+++ tests/LH_MicroTest.cpp
@@ -64,13 +64,14 @@ void prepare() {
 }
 
 void restart() {
-    cout << "destroy" << endl;
+    cout << "\tdestroy" << endl;
     level_destroy(levelHash);
-    cout << "reinit" << endl;
+    cout << "\treinit" << endl;
     levelHash = level_init(DEFAULT_HASH_LEVEL);
     for (int i = 0; i < thread_number; i++) {
         parms[i].levelHash = levelHash;
     }
+    cout << "\trestarted" << endl;
 }
 
 void finish() {
@@ -104,6 +105,43 @@ void simpleInsert() {
     found = 0;
     for (int i = 0; i < total_count; i += DEFAULT_KEY_LENGTH) {
         found += (0 == level_delete(levelHash, &loads[i * DEFAULT_KEY_LENGTH]));
+    }
+    cout << found << " " << tracer.getRunTime() << endl;
+}
+
+void uniqueInsert() {
+    uint8_t *uniques = (uint8_t *) calloc(DEFAULT_KEY_LENGTH * total_count, sizeof(uint8_t));
+    char buf[DEFAULT_STR_LENGTH];
+    for (int i = 0; i < total_count; i++) {
+        std::sprintf(buf, "%d", i);
+        memcpy(uniques + i * DEFAULT_KEY_LENGTH, buf, DEFAULT_KEY_LENGTH);
+
+    }
+    Tracer tracer;
+    tracer.startTime();
+    int inserted = 0;
+    for (int i = 0; i < total_count; i++) {
+        if (!level_insert(levelHash, &uniques[i * DEFAULT_KEY_LENGTH], &uniques[i * DEFAULT_KEY_LENGTH])) {
+            inserted++;
+        }
+        if (false && (i % 100000 == 0 || (i - 10000000) % 100 == 1)) {
+            memset(buf, 0, DEFAULT_STR_LENGTH);
+            memcpy(buf, &uniques[i * DEFAULT_KEY_LENGTH], DEFAULT_KEY_LENGTH);
+            cout << i << " " << buf << " " << uniques[i * DEFAULT_KEY_LENGTH] << endl;
+        }
+    }
+    cout << inserted << " " << tracer.getRunTime() << endl;
+    tracer.startTime();
+    uint64_t found = 0;
+    uint8_t value[DEFAULT_KEY_LENGTH];
+    for (int i = 0; i < total_count; i += DEFAULT_KEY_LENGTH) {
+        found += (0 == level_query(levelHash, &uniques[i * DEFAULT_KEY_LENGTH], value));
+    }
+    cout << found << " " << tracer.getRunTime() << endl;
+    tracer.startTime();
+    found = 0;
+    for (int i = 0; i < total_count; i += DEFAULT_KEY_LENGTH) {
+        found += (0 == level_delete(levelHash, &uniques[i * DEFAULT_KEY_LENGTH]));
     }
     cout << found << " " << tracer.getRunTime() << endl;
 }
@@ -193,8 +231,14 @@ int main(int argc, char **argv) {
     loads = (uint8_t *) calloc(DEFAULT_KEY_LENGTH * total_count, sizeof(uint8_t));
     UniformGen<uint8_t>::generate(loads, DEFAULT_KEY_LENGTH, key_range, total_count);
     levelHash = level_init(DEFAULT_HASH_LEVEL);
+    cout << "simple" << endl;
     simpleInsert();
     prepare();
+    cout << "restart" << endl;
+    restart();
+    cout << "unique" << endl;
+    uniqueInsert();
+    //prepare();
     cout << "restart" << endl;
     restart();
     cout << "multiinsert" << endl;
