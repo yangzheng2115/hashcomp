9a8e558895f0fece9a0ecd1d0c7466bf806f3cb3
lwhay <lwh@whu.edu.cn>
2019-05-23 12:35:39 +0800

    revised faster namespace.

---
+++ src/faster/api/kvcontext.h
@@ -0,0 +1,17 @@
+//
+// Created by lwh on 19-5-23.
+//
+
+#ifndef HASHCOMP_KVCONTEXT_H
+#define HASHCOMP_KVCONTEXT_H
+
+namespace FASTER {
+    namespace api {
+        class kvcontext {
+
+        };
+    }
+}
+
+
+#endif //HASHCOMP_KVCONTEXT_H
