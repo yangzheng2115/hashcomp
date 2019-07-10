7254801ae99d5c5c8adf034ce5751fd19ab952ff
lwhay <lwh@whu.edu.cn>
2019-07-11 00:56:41 +0800

    adapt to hp servers.

--- utils/UH_UnitTest.cpp
+++ utils/UH_UnitTest.cpp
@@ -10,6 +10,12 @@
 #include "tracer.h"
 #include "universal_hash_table.h"
 
+#if defined(__linux__)
+
+#include <cpuid.h>
+
+#endif
+
 using namespace std;
 using namespace boost;
 using namespace neatlib;
@@ -95,40 +101,41 @@ void stackWorker(S &s) {
     }
 }*/
 
+const size_t total_item = (1 << 26);
+
+const int thread_number = 8;
+
 void stdWorker(std::stack<int> *s) {
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < total_item; i++) {
         s->push(i);
     }
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < total_item; i++) {
         s->pop();
     }
 }
 
 void bstWorker(boost::lockfree::stack<int> *s) {
-    for (int i = 0; i < 100000000; i++) {
+    for (int i = 0; i < total_item / thread_number; i++) {
         s->push(i);
     }
-    for (int i = 0; i < 100000000; i++) {
+    for (int i = 0; i < total_item / thread_number; i++) {
         int v = -1;
         s->pop(v);
     }
 }
 
-const int thread_number = 1;
-
 void boostStackVSstdStack(bool usingStd) {
-    std::thread *workers[thread_number];
+    std::vector<std::thread> workers;
     if (usingStd) {
         std::stack<int> stdStack;
         stdWorker(&stdStack);
     } else {
         boost::lockfree::stack<int> bstStack(128);
         for (int t = 0; t < thread_number; t++) {
-            std::thread worker(bstWorker, &bstStack);
-            workers[t] = &worker;
+            workers.push_back(std::thread(bstWorker, &bstStack));
         }
         for (int t = 0; t < thread_number; t++) {
-            workers[t]->join();
+            workers[t].join();
         }
     }
 }
@@ -138,11 +145,16 @@ int main(int argc, char **argv) {
     tracer.startTime();
     simpleOperationTests();
     cout << "Ist: " << tracer.getRunTime() << endl;
-    tracer.startTime();
-    boostStackVSstdStack(true);
-    cout << "Std: " << tracer.getRunTime() << endl;
-    tracer.startTime();
-    boostStackVSstdStack(false);
-    cout << "Bst: " << tracer.getRunTime() << endl;
+#if defined(__linux__)
+    cout << "CPUs: " << sysconf(_SC_NPROCESSORS_ONLN) << endl;
+    if (sysconf(_SC_NPROCESSORS_ONLN) > 8) {
+        tracer.startTime();
+        boostStackVSstdStack(true);
+        cout << "Std: " << tracer.getRunTime() << endl;
+        tracer.startTime();
+        boostStackVSstdStack(false);
+        cout << "Bst: " << tracer.getRunTime() << endl;
+    }
+#endif
     return 0;
 }
