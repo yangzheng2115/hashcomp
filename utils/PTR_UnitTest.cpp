b56f1f7c682e9f3743e5f315ddc3b0a362dcef9e
Michael <lwhaywhu@gmail.com>
2019-09-23 03:20:08 +0800

    local boosting atomic.

--- utils/PTR_UnitTest.cpp
+++ utils/PTR_UnitTest.cpp
@@ -21,7 +21,7 @@ constexpr int threadOprs = (1 << 2);
 
 constexpr long iteration = (1 << 30);
 
-struct node {
+struct alignas(128)node {
     uint64_t element = 0;
 public:
     node() : element(0) {}
@@ -58,7 +58,7 @@ public:
 struct node wallclock(0);
 
 //unique_ptr<node> pwc = unique_ptr<node>(new node(0), std::default_delete<node>());
-volatile atomic<node *> pwc(&wallclock);
+atomic<node *> pwc(&wallclock);
 stringstream *output;
 
 void uniquePtrWorker(int tid) {
@@ -71,15 +71,15 @@ void uniquePtrWorker(int tid) {
     ////auto pcw = static_cast<node *>(pwc.get());
     //*/
 
-    register int yc = 1;
+    int yc = 1;
     for (long r = 0; r < (iteration / pdegree); r++) {
-        register node *dummy = nullptr;
+        node *dummy = nullptr;
         /*register node *pcw = nullptr;
         do {
             pcw = pwc;
         } while (pcw == nullptr || !pwc.compare_exchange_weak(pcw, dummy, memory_order_release));*/
 
-        register node *pcw = &wallclock;
+        node *pcw = &wallclock;
         while (!pwc.compare_exchange_strong(pcw, dummy)) {
             for (int c = 0; c < yc; c++) this_thread::yield();
             yc *= 2;
@@ -145,18 +145,62 @@ void uniquePtrTests() {
 
 struct node *wallclocks;
 
-volatile atomic<node *> *pwcs;
+template<class _Tp>
+struct alignas(128) alignasatomic : public atomic<_Tp> {
+public:
+    alignasatomic() {};
+
+    //alignasatomic(_Tp e) : atomic<_Tp>(e) {}
+
+    bool compare_exchange_strong(_Tp &__e, _Tp __d,
+                                 memory_order __m = memory_order_seq_cst) _NOEXCEPT {
+        return compare_exchange_strong(__e, __d, __m);
+    }
+
+    alignasatomic &operator=(alignasatomic &n) {
+        this->store(n);
+        return *this;
+    }
+
+    alignasatomic &operator=(const alignasatomic &n) {
+        this->store(n);
+        return *this;
+    }
+
+    alignasatomic &operator=(_Tp &n) {
+        this->store(n);
+        return *this;
+    }
+
+    alignasatomic &operator=(const _Tp &n) {
+        this->store(n);
+        return *this;
+    }
+
+    alignasatomic *operator=(alignasatomic *n) {
+        this->store(*n);
+        return this;
+    }
+
+    alignasatomic *operator=(const alignasatomic *n) {
+        this->store(*n);
+        return this;
+    }
+};
+
+alignas(128)atomic<node *> *pwcs;
 
 void uniquePtrPWorker(int tid) {
     Tracer tracer;
     tracer.startTime();
+    atomic<node *> pc(&wallclocks[tid]);
 
-    register int yc = 1;
+    int yc = 1;
     for (long r = 0; r < (iteration / pdegree); r++) {
-        register node *dummy = nullptr;
+        node *dummy = nullptr;
 
-        register node *pcw = wallclocks + tid;
-        while (!pwcs[tid].compare_exchange_strong(pcw, dummy)) {
+        node *pcw = wallclocks + tid;
+        while (!/*pwcs[tid]*/pc.compare_exchange_strong(pcw, dummy)) {
             for (int c = 0; c < yc; c++) this_thread::yield();
             yc *= 2;
             pcw = wallclocks + tid;
@@ -166,16 +210,17 @@ void uniquePtrPWorker(int tid) {
         for (int i = 0; i < threadOprs; i++) {
             pcw->element++;
         }
-        pwcs[tid].store(pcw);
+        /*pwcs[tid]*/pc.store(pcw);
+        //if (r % 1000000 == 0) cout << tid << ":" << r << endl;
     }
     output[tid] << tid << ":" << tracer.getRunTime() << endl;
 }
 
 void uniquePtrPTests() {
     wallclocks = new node[pdegree];
-    pwcs = new atomic<node *>[pdegree];
-    for (int i = 0; i < pdegree; i++) {
-        pwcs[i].store(wallclocks + i);
+    pwcs = new atomic<node *>[pdegree]alignas(128);
+    for (int t = 0; t < pdegree; t++) {
+        pwcs[t] = wallclocks + t;
     }
     delete[] output;
     output = new stringstream[pdegree];
@@ -194,8 +239,8 @@ void uniquePtrPTests() {
     }
 
     cout << "Multi-thread: " << pdegree << "<->" << ec << "<>" << ":" << tracer.getRunTime() << endl;
-    delete[] pwcs;
     delete[] wallclocks;
+    delete[] pwcs;
 }
 
 int main(int argc, char **argv) {
