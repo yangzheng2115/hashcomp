63dd4b71d4df2f868b0fb4fcf15c1724d5263b7b
lwhay <lwh@whu.edu.cn>
2019-10-08 16:29:24 -0700

    full consistency test.

--- tests/FH_Unit/faster_function_test.cpp
+++ tests/FH_Unit/faster_function_test.cpp
@@ -5,6 +5,7 @@
 #include <atomic>
 #include <iostream>
 #include <thread>
+#include <time.h>
 #include "faster.h"
 #include "gtest/gtest.h"
 #include "cvkvcontext.h"
@@ -15,12 +16,14 @@ using namespace FASTER::io;
 using namespace FASTER::core;
 
 atomic<uint64_t> tick;
+atomic<uint64_t> tickMulti;
+constexpr uint64_t rounds = (1 << 20);
+constexpr uint64_t bfsize = 256;
+constexpr uint64_t insize = 64;
 
 TEST(ContextVerify, ConcurrentRU) {
     typedef QueueIoHandler handler_t;
     typedef FileSystemDisk<handler_t, 1073741824ull> disk_t;
-    constexpr uint64_t rounds = (1 << 20);
-    constexpr uint64_t bfsize = 256;
     using store_t = FasterKv<Key, Value, disk_t>;
     size_t init_size = next_power_of_two(512 / 2);
     store_t store{init_size, 17179869184, "storage"};
@@ -59,7 +62,7 @@ TEST(ContextVerify, ConcurrentRU) {
             Status stat = store->Read(rc, callback, 1);
 
             // Nice print
-            if (i % 8 == 0 && i != 0) cout << endl;
+            if (i % 8 == 0) cout << endl;
             cout.width(7);
             cout << tick->load();
             cout;
@@ -80,6 +83,99 @@ TEST(ContextVerify, ConcurrentRU) {
     std::thread readThread(reader, &store, &tick);
     updateThread.join();
     readThread.join();
+}
+
+TEST(ContextVerify, NanoSleep) {
+    timespec timespec;
+    timespec.tv_sec = 0;
+    timespec.tv_nsec = 1;
+    timeval begin, end;
+    gettimeofday(&begin, nullptr);
+    for (int i = 0; i < 1000; i++) nanosleep(&timespec, nullptr);
+    gettimeofday(&end, nullptr);
+    cout << (end.tv_sec - begin.tv_sec) * 1000000 + end.tv_usec - begin.tv_usec << endl;
+    gettimeofday(&begin, nullptr);
+    for (int i = 0; i < 1000; i++) usleep(1000);
+    gettimeofday(&end, nullptr);
+    cout << (end.tv_sec - begin.tv_sec) * 1000000 + end.tv_usec - begin.tv_usec << endl;
+}
+
+TEST(ContextVerify, ConcurrentRUMultiBytes) {
+    typedef QueueIoHandler handler_t;
+    typedef FileSystemDisk<handler_t, 1073741824ull> disk_t;
+    using store_t = FasterKv<Key, Value, disk_t>;
+    size_t init_size = next_power_of_two(512 / 2);
+    store_t store{init_size, 17179869184, "storage"};
+    Value loads[bfsize];
+    atomic_bool begin(false);
+    uint8_t **values = new uint8_t *[bfsize];
+    for (uint64_t i = 0; i < bfsize; i++) {
+        values[i] = new uint8_t[insize];
+        for (int j = 0; j < insize / sizeof(uint64_t); j++) {
+            *(uint64_t *) (values[i] + j * insize / sizeof(uint64_t)) = bfsize - i % bfsize;
+        }
+        loads[i].reset(values[i], insize);
+    }
+
+    auto updater = [](Value *loads, store_t *store, atomic<uint64_t> *tick, atomic_bool *begin) {
+        while (!begin->load()) this_thread::yield();
+        for (int i = 0; i < rounds; i++) {
+            auto callback = [](IAsyncContext *ctxt, Status result) {
+                CallbackContext<UpsertContext> context{ctxt};
+            };
+            UpsertContext context(0, insize);
+            context.reset(loads[i % bfsize].get());
+            uint64_t *value = (uint64_t *) (loads[i % bfsize].get());
+            ASSERT_EQ(*value, 256 - i % 256);
+            Status stat = store->Upsert(context, callback, 1);
+            tick->fetch_add(1);
+            //usleep(5);
+        }
+    };
+
+    auto reader = [](store_t *store, atomic<uint64_t> *tick, atomic_bool *begin) {
+        uint64_t oldtick = 0;
+        uint64_t i = 0;
+        begin->store(true);
+        cout << i << "<->" << tick->load() << endl;
+        while (tick->load() < rounds) {
+            auto callback = [](IAsyncContext *ctxt, Status result) {
+                CallbackContext<ReadContext> context{ctxt};
+            };
+            ReadContext rc(0);
+            Status stat = store->Read(rc, callback, 1);
+            if (tick->load() - oldtick < (1 << 14)) continue;
+            oldtick = tick->load();
+            // Nice print
+            if (i % 8 == 0) cout << endl;
+            cout.width(7);
+            cout << tick->load();
+            cout;
+            cout << ":";
+            cout.width(2);
+            cout << i++;
+            cout;
+            cout << "<->";
+            cout.width(3);
+            cout << *((uint64_t *) rc.output_bytes);
+            cout;
+            cout << " ";
+            for (int j = 0; j < insize / sizeof(uint64_t); j++) {
+                ASSERT_EQ(*(uint64_t *) (rc.output_bytes + j * insize / sizeof(uint64_t)),
+                          *((uint64_t *) rc.output_bytes));
+            }
+        }
+        cout << endl << i << "<->" << tick->load() << endl;
+    };
+
+    std::thread updateThreads[4];
+    for (int i = 0; i < 4; i++)
+        updateThreads[i] = std::thread(updater, loads, &store, &tickMulti, &begin);
+    std::thread readThread(reader, &store, &tickMulti, &begin);
+    for (int i = 0; i < 4; i++)
+        updateThreads[i].join();
+    readThread.join();
+    delete[] values;
 }
 
 int main(int argc, char **argv) {
