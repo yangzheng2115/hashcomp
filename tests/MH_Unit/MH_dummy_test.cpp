027c4755e0574c6e79345451a5fc8f8427678937
Michael <lwhaywhu@gmail.com>
2019-10-11 22:38:27 +0800

    Add dummy unittest.

---
+++ tests/MH_Unit/MH_dummy_test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Michael on 2019-10-11.
+//
+
+#include "gtest/gtest.h"
+
+TEST(DummyTest, UnionStructTest) {
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
