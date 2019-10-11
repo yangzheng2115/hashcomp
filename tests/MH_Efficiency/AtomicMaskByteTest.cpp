3c81d4c476cddd64ca3260a73f17755f3cdcdccc
Michael <lwhaywhu@gmail.com>
2019-10-12 01:35:45 +0800

    Counting it.

--- tests/MH_Efficiency/AtomicMaskByteTest.cpp
+++ tests/MH_Efficiency/AtomicMaskByteTest.cpp
@@ -15,22 +15,28 @@
 
 using namespace std;
 
-uint64_t TotalPayloads = (1LLU << 20);
+uint64_t TotalPayloads = (1LLU << 24);
 uint64_t ThreadsNumber = 4;
 uint64_t LoadPerThread = TotalPayloads / ThreadsNumber;
 int usingSub = true;
 
+atomic<uint64_t> actualCount(0);
+
 AtomicMaskByte amb;
 
 void DWordAdd() {
+    uint64_t tick = 0;
     for (uint64_t i = 0; i < LoadPerThread; i++) {
         amb.dword.fetch_add(1);
         if (usingSub) amb.dword.fetch_sub(1);
+        tick++;
     }
+    actualCount.fetch_add(tick);
 }
 
 // use macro instead
 void ByteAdd(int &idx) {
+    uint64_t tick = 0;
     for (uint64_t i = 0; i < LoadPerThread; i++) {
         switch (idx % 8) {
             case 0:
@@ -68,7 +74,9 @@ void ByteAdd(int &idx) {
             default:
                 return;
         }
+        tick++;
     }
+    actualCount.fetch_add(tick);
 }
 
 int main(int argc, char **argv) {
@@ -76,6 +84,7 @@ int main(int argc, char **argv) {
         ThreadsNumber = std::atol(argv[1]);
         TotalPayloads = std::atol(argv[2]);
         usingSub = std::atoi(argv[3]);
+        cout << "Thread: " << argv[1] << " total: " << argv[2] << " sub: " << argv[3] << endl;
     }
     cout << "Thread: " << ThreadsNumber << " total: " << TotalPayloads << " sub: " << usingSub << endl;
     amb.dword = 0;
@@ -89,8 +98,9 @@ int main(int argc, char **argv) {
     for (int i = 0; i < ThreadsNumber; i++) {
         workers[i].join();
     }
-    cout << "DWord: " << tracer.getRunTime() << endl;
+    cout << "DWord: " << tracer.getRunTime() << "<->" << actualCount.load() << endl;
 
+    actualCount.store(0);
     tracer.startTime();
 
     thread byters[ThreadsNumber];
@@ -100,6 +110,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < ThreadsNumber; i++) {
         byters[i].join();
     }
-    cout << "Bytes: " << tracer.getRunTime() << endl;
+    cout << "Bytes: " << tracer.getRunTime() << "<->" << actualCount.load() << endl;
     return 0;
 }
