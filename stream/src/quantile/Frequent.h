7a872d640b5d97709a6dbe6135b141996d73cb1a
Michael <lwhaywhu@gmail.com>
2019-10-15 01:40:19 +0800

    Add quantiles

---
+++ stream/src/quantile/Frequent.h
@@ -0,0 +1,8 @@
+//
+// Created by Michael on 2019-10-15.
+//
+
+#ifndef HASHCOMP_FREQUENT_H
+#define HASHCOMP_FREQUENT_H
+
+#endif //HASHCOMP_FREQUENT_H
---
+++ stream/tests/GK_UnitTest.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Michael on 2019-10-15.
+//
+
+#include "gtest/gtest.h"
+
+TEST(GKUnitTest, Operations) {
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
