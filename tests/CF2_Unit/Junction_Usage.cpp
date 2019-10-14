869e85f691d4a1eb5d49cea4b02cfb321cbfc65a
Michael <lwhaywhu@gmail.com>
2019-10-14 17:04:56 +0800

    Error on junction.

--- tests/CF2_Unit/Junction_Usage.cpp
+++ tests/CF2_Unit/Junction_Usage.cpp
@@ -10,11 +10,27 @@
 #include "gtest/gtest.h"
 #include "junction/QSBR.h"
 #include "junction/ConcurrentMap_Leapfrog.h"
+#include "junction/ConcurrentMap_Grampa.h"
 
-TEST(JunctionTests, UnitOperations) {
+TEST(JunctionTests, LeapfrogOperations) {
     junction::ConcurrentMap_Leapfrog<uint64_t, uint64_t> jmap(128);
     jmap.assign(1, 1);
-    ASSERT_EQ(jmap.get(1), 1);
+    auto v = jmap.get(1);
+    ASSERT_EQ(v, 1);
+    jmap.exchange(1, 2);
+    jmap.find(1);
+    ASSERT_EQ(jmap.get(1), 2);
+}
+
+TEST(JunctionTests, GrampaOperations) {
+    junction::QSBR::Context context = junction::DefaultQSBR.createContext();
+    junction::ConcurrentMap_Grampa<int, int> jmap(128);
+    for (int i = 0; i < 128; i++) jmap.assign(i, i);
+    jmap.find(1);
+    ASSERT_EQ(jmap.get(1), 2);
+    jmap.exchange(1, 2);
+    jmap.find(1);
+    ASSERT_EQ(jmap.get(1), 2);
 }
 
 int main(int argc, char **argv) {
